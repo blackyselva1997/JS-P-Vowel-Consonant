89827f4b0dd866351d4b6b59484c6f06a04ce2ba
blackyselva <selvam17196@gmail.com>
2023-03-19 21:12:23 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,7 @@
+let alpha = String(prompt("Enter a Alphabet only one"));
+
+if (alpha === "a" || alpha === "e" || alpha === "i" || alpha === "o" || alpha === "u" || alpha === "A" || alpha === "E" || alpha === "I" || alpha === "O" || alpha === "U") {
+    document.write("You enter a Vowel");
+} else {
+    document.write("You enter a consonant");
+}
